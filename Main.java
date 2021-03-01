6892dfe354087119ba4ad4cf268cd06ca4c6b629
Tristan <tristaneckenbach@gmail.com>
2021-03-01 11:31:55 +0000

    Array mit 10 Zufallszahlen gefüllt

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Random zufi = new Random();
+    
+    int[] zahlen = new int[10];
+    for (int i = 0; i< zahlen.length; i++){
+      zahlen[i] = zufi.nextInt(100);
+      System.out.print(zahlen[i] + " ");
+    }
   }
 }
